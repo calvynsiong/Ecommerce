b0ec8a5814b14e6c0f5748c69bb45f76d8885c3d
Calvyn <calvyn360@gmail.com>
2021-05-21 18:49:09 -0400

    Updating changes

--- README.md
+++ README.md
@@ -1,11 +1,58 @@
+# SETUP
+$ mkdir client server
+$ npm i express mongoose dotenv
+$ npm i --save-dev nodemon concurrently
+
+# FRONT END
+cd client
+npx create-react-app
+rm -rf .git (To remove git repo in client folder)
+
+# BACKEND
+"change main in package.json to server.js"
+
+## In server.js
+
+const express = require('express');
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+
+})
+
+## .env + gitignore
+
+PORT = 5000
+MONGO_URI = ""
+==============================
+
+### dependencies
+node_modules
+node_modules/
+/.pnp
+.pnp.js
+
+### testing
+/coverage
+
+### production
+/build
+
+### misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
 
 
-To use this application, 
+### Testing
 
-1. make dev.js file inside config folder 
-2. put mongoDB info into dev.js file 
-3. Type  " npm install " inside the root directory  ( Download Server Dependencies ) 
-4. Type " npm install " inside the client directory ( Download Front-end Dependencies )
-
-
-
+node server/server.js
--- client/src/index.js
+++ client/src/index.js
@@ -1,36 +1,17 @@
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/ie11';
-import 'core-js';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from "react-router-dom";
-
-import Reducer from './_reducers';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk';
-
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Provider
-        store={createStoreWithMiddleware(
-            Reducer,
-            window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-        )}
-    >
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
-    , document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
