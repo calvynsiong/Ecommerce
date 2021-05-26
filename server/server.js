1e74466bbc64cc8e0b43aa0377538919f5d4eea3
Calvyn <calvyn360@gmail.com>
2021-05-25 22:02:14 -0400

    Trying to make backend work

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.use("/api/products", productRoutes);
 // ! Deployment
 
 
+
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/build")));
 
