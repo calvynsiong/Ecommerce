a9b3bde6d1cc84e00051fcfe25a5f8a5da87e679
Calvyn <calvyn360@gmail.com>
2021-05-26 11:14:04 -0400

    Trying new scripts

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.use("/api/products", productRoutes);
 
 // ! Deployment
 
-console.log(path.join(__dirname, "client/build"))
+console.log(path.join(__dirname, "/client/build"))
 console.log(path.join(__dirname, "client", "build", "index.html"))
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "/client/build")));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, "client", "build", "index.html"))
